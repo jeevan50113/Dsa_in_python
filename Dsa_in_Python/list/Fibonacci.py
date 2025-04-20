b969973d995c5a41b82a414292937e01e54a35cf
UNQ CODER <65815018+jeevan50113@users.noreply.github.com>
2025-04-20 22:14:42 +0530

    Update Fibonacci.py

--- Dsa_in_Python/list/Fibonacci.py
+++ Dsa_in_Python/list/Fibonacci.py
@@ -1,9 +1,12 @@
-# fibonacci.py
-def fib(n):
-    if n <= 1:
-        return n
-    return fib(n - 1) + fib(n - 2)
+def fibonacci_list(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib
 
-for i in range(6):
-    print(fib(i), end=" ")
-# Output: 0 1 1 2 3 5
+print(fibonacci_list(10))
+# Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
